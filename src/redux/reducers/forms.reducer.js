5423935c2e99f31991d6dd64d68ab32ab9337751
Travis Huss <travisjhuss@gmail.com>
2021-03-08 15:44:43 -0600

    building out providerAnswers reducer

--- src/redux/reducers/forms.reducer.js
+++ src/redux/reducers/forms.reducer.js
@@ -17,31 +17,64 @@ const clientAnswers = (
   action
 ) => {
   switch (action.type) {
-    case 'SET_PERSONAL_DETAILS':
+    case 'SET_CLIENT_PERSONAL_DETAILS':
       return { ...state, [action.payload.key]: action.payload.value };
     case 'SET_CLIENT_PREFERENCES':
-        const preferenceId = action.payload;
-        if (state.preferences.indexOf(preferenceId) === -1) {
-            return {
-                ...state,
-                preferences: [...state.preferences, id]
-            };
-        } else {
-            return {
-                ...state,
-                preferences: state.preferences.filter((index) => index !== id)
-            };
-        }
+      const preferenceId = action.payload;
+      if (state.preferences.indexOf(preferenceId) === -1) {
+        return {
+          ...state,
+          preferences: [...state.preferences, id],
+        };
+      } else {
+        return {
+          ...state,
+          preferences: state.preferences.filter((index) => index !== id),
+        };
+      }
     default:
       return state;
   }
 };
 
 // Holds providers answers as they step through form
-const providerAnswers = (state = {}, action) => {
+const providerAnswers = (
+  state = {
+    first_name: '',
+    last_name: '',
+    pic: '',
+    video: '',
+    location: '',
+    date_of_birth: '',
+    pronouns: '',
+    background: '',
+    strengths: '',
+    approach: '',
+    insurance: false,
+    sliding_scale: false,
+    preferences: [],
+    questions: [],
+  },
+  action
+) => {
   switch (action.type) {
-    case '':
-      return '';
+    case 'SET_PROVIDER_PERSONAL_DETAILS':
+      return { ...state, [action.payload.key]: action.payload.value };
+    case 'SET_PROVIDER_PREFERENCES':
+      const preferenceId = action.payload;
+      if (state.preferences.indexOf(preferenceId) === -1) {
+        return {
+          ...state,
+          preferences: [...state.preferences, id],
+        };
+      } else {
+        return {
+          ...state,
+          preferences: state.preferences.filter((index) => index !== id),
+        };
+      };
+    case 'SET_PROVIDER_QUESTIONS':
+        
     default:
       return state;
   }
