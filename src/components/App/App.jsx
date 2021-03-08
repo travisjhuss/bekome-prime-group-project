fc63834bee764c201bd2308fdd34e8e00754c493
Connor Detviler <connor.detviler@gmail.com>
2021-03-08 09:52:47 -0600

    bringing back stash

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import ExploreView from '../ExploreView/ExploreView';
 
 import './App.css';
 
@@ -100,6 +101,17 @@ function App() {
             authRedirect="/user"
           >
             <LandingPage />
+          </ProtectedRoute>
+
+          <ProtectedRoute
+            // with authRedirect:
+            // - if logged in, redirects to "/user"
+            // - else shows ExploreView at "/explore"
+            exact
+            path="/explore"
+            // authRedirect="/user"
+          >
+            <ExploreView />
           </ProtectedRoute>
 
           {/* If none of the other routes matched, we will show a 404. */}
