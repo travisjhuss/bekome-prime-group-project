714f33a0383379215c69f9db7aaaa3223f2ad13b
Travis Huss <travisjhuss@gmail.com>
2021-03-08 11:11:31 -0600

    clientAnswers reducer fleshed out to handle changes on form

--- src/redux/reducers/forms.reducer.js
+++ src/redux/reducers/forms.reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-//
+// Holds preference options for populating form
 const preferences = (state = [], action) => {
   switch (action.type) {
     case 'SET_PREFERENCES':
@@ -10,6 +10,7 @@ const preferences = (state = [], action) => {
   }
 };
 
+//  Holds provider questions for populating form
 const providerQuestions = (state = [], action) => {
   switch (action.type) {
     case 'SET_PROVIDER_QUESTIONS':
@@ -19,17 +20,44 @@ const providerQuestions = (state = [], action) => {
   }
 };
 
-// registrationMessage holds the string that will display
-// on the registration screen if there's an error
-const clientAnswers = (state = {}, action) => {
+// Holds clients answers as they step through form
+const clientAnswers = (
+  state = {
+    first_name: '',
+    last_name: '',
+    pic: '',
+    date_of_birth: '',
+    pronouns: '',
+    location: '',
+    primary_reason: '',
+    previous_therapy: false,
+    previous_experience: '',
+    preferences: [],
+  },
+  action
+) => {
   switch (action.type) {
-    case '':
-      return '';
+    case 'SET_PERSONAL_DETAILS':
+      return { ...state, [action.payload.key]: action.payload.value };
+    case 'SET_CLIENT_PREFERENCES':
+        const preferenceId = action.payload;
+        if (state.preferences.indexOf(preferenceId) === -1) {
+            return {
+                ...state,
+                preferences: [...state.preferences, id]
+            };
+        } else {
+            return {
+                ...state,
+                preferences: state.preferences.filter((index) => index !== id)
+            };
+        }
     default:
       return state;
   }
 };
 
+// Holds providers answers as they step through form
 const providerAnswers = (state = {}, action) => {
   switch (action.type) {
     case '':
