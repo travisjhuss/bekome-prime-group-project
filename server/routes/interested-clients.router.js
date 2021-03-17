069dd2a60677b9b7071ce9624a7224b1f57b231d
Napoleon Stanley <napoleonstanley316@gmail.com>
2021-03-17 14:49:53 -0500

    edited interersted-clients router

--- server/routes/interested-clients.router.js
+++ server/routes/interested-clients.router.js
@@ -41,13 +41,3 @@ router.post('/', (req, res) => {
 
 module.exports = router;
 
-
-
-
-// SELECT "clients".clients_users_id, "clients".first_name, "clients".last_name, "clients".city, clients.state, "clients".pic, "clients".primary_reason,
-// ARRAY_AGG("clients_preferences".preferences_id) AS "preferences_array" 
-// FROM "clients"
-// JOIN "clients_providers_favs" ON "clients_providers_favs".clients_users_id = "clients".clients_users_id
-// JOIN "clients_preferences" ON "clients_preferences".clients_users_id = "clients".clients_users_id
-// WHERE "clients_providers_favs".providers_users_id = $1
-// GROUP BY clients.clients_users_id, clients.first_name, clients.last_name, clients.city, clients.state, clients.pic, clients.primary_reason;
