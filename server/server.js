d18fc59cbec16a15199c75011ef0c883eda75c07
Napoleon Stanley <napoleonstanley316@gmail.com>
2021-03-17 14:52:49 -0500

    added routes to server.js

--- server/server.js
+++ server/server.js
@@ -13,6 +13,9 @@ const formsRouter = require('./routes/forms.router');
 const exploreRouter = require('./routes/explore.router');
 const providerDetailsRouter = require('./routes/providerDetails.router');
 const favoriteRouter = require('./routes/favorite.router');
+const savedProvidersRouter = require('./routes/savedProviders.router');
+const s3Router = require('./routes/s3.router');
+const editProfileRouter = require('./routes/editProfile.router');
 const interestedClientsRouter = require('./routes/interested-clients.router')
 
 // Body parser middleware
@@ -32,6 +35,9 @@ app.use('/api/forms', formsRouter);
 app.use('/api/explore', exploreRouter);
 app.use('/api/provider-details', providerDetailsRouter);
 app.use('/api/favorite', favoriteRouter);
+app.use('/api/saved-providers', savedProvidersRouter)
+app.use('/s3', s3Router);
+app.use('/api/edit', editProfileRouter)
 app.use('/api/interested-clients', interestedClientsRouter);
 
 // Serve static files
