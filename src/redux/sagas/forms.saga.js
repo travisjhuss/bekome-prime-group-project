c92863326a2ddc0886008cfae0411e9ee4f3df33
Travis Huss <travisjhuss@gmail.com>
2021-03-24 10:43:54 -0500

    sagas commented

--- src/redux/sagas/forms.saga.js
+++ src/redux/sagas/forms.saga.js
@@ -1,6 +1,9 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
+// These sagas handle the forms as well as admin currently
+// the admin sagas should go in their own file but we ran out of time
+
 function* formsSaga() {
   yield takeEvery('FETCH_PREFERENCES', fetchPreferences);
   yield takeEvery('ADD_NEW_PREFERENCE', addNewPreference);
@@ -30,7 +33,6 @@ function* fetchProviderQuestions() {
   try {
     // GET all preferences from DB to populate forms
     const response = yield axios.get('/api/forms/provider_questions');
-    console.log('fetchProviderQuestions response.data', response.data);
     // save in preferences reducer
     yield put({ type: 'SET_PROVIDER_QUESTIONS', payload: response.data });
   } catch (err) {
@@ -40,44 +42,43 @@ function* fetchProviderQuestions() {
 
 function* addNewQuestion(action) {
   try {
-    console.log('in saga new question', action.payload)
-    yield axios.post('/api/forms/add_question', {content: action.payload});
+    // adds question to provider form
+    yield axios.post('/api/forms/add_question', { content: action.payload });
     // fetch provider questions so the admin form updates
-    yield put({ type: 'FETCH_PROVIDER_QUESTIONS' })
+    yield put({ type: 'FETCH_PROVIDER_QUESTIONS' });
   } catch (err) {
-    console.log('error in addNewQuestion', err)
+    console.log('error in addNewQuestion', err);
   }
 }
 
+//
 function* deleteQuestion(action) {
   try {
-    console.log('in saga delete question', action.payload)
-    yield axios.delete(`/api/forms/delete_question/${action.payload}`)
-
+    // deletes question from provider form
+    yield axios.delete(`/api/forms/delete_question/${action.payload}`);
     // fetch provider questions so the admin form updates
-    yield put({ type: 'FETCH_PROVIDER_QUESTIONS' })
+    yield put({ type: 'FETCH_PROVIDER_QUESTIONS' });
   } catch (err) {
-    console.log('error in deleteQuestion', err)
+    console.log('error in deleteQuestion', err);
   }
 }
 
 function* editQuestion(action) {
   try {
-    console.log('in editQuestion with payload: ', action.payload);
+    // will edit question from provider form
     yield axios.put('/api/forms/edit_question', action.payload);
-
-    yield put({type: 'FETCH_PROVIDER_QUESTIONS'});
-  } catch(err) {
-    console.log('error in editQuestion', err)
+    yield put({ type: 'FETCH_PROVIDER_QUESTIONS' });
+  } catch (err) {
+    console.log('error in editQuestion', err);
   }
 }
 
 function* addNewClient(action) {
   try {
-    console.log('in addNewClient with payload:', action.payload);
+    // adds new client to db
     yield axios.post('/api/forms/add_client', action.payload);
     // fetch user so nav bar updates
-    yield put({type: 'FETCH_USER'});
+    yield put({ type: 'FETCH_USER' });
   } catch (err) {
     console.log('error in addNewClient:', err);
   }
@@ -85,10 +86,10 @@ function* addNewClient(action) {
 
 function* addNewProvider(action) {
   try {
-      console.log('in addNewProvider with payload:', action.payload);
-      yield axios.post('/api/forms/add_provider', action.payload);
-      // fetch user so nav bar updates
-      yield put({type: 'FETCH_USER'});
+    // adds new provider to db
+    yield axios.post('/api/forms/add_provider', action.payload);
+    // fetch user so nav bar updates
+    yield put({ type: 'FETCH_USER' });
   } catch (err) {
     console.log('error in addNewProvider:', err);
   }
@@ -96,22 +97,22 @@ function* addNewProvider(action) {
 
 function* addNewPreference(action) {
   try {
-    console.log('in addNewPreference with payload:', action.payload);
+    // adds new preference to db
     yield axios.post('/api/forms/add_preference', action.payload);
     // fetch preferences so admin panel updates
-    yield put({type: 'FETCH_PREFERENCES'});
+    yield put({ type: 'FETCH_PREFERENCES' });
   } catch (err) {
     console.log('error in addNewPreference', err);
   }
 }
 
 function* deletePreference(action) {
-  const id = action.payload
+  const id = action.payload;
   try {
-    console.log('in deletePreference with payload:', action.payload);
+    // deletes preference from db
     yield axios.delete(`/api/forms/delete_preference/${id}`);
     // fetch preferences so admin panel updates
-    yield put({type: 'FETCH_PREFERENCES'});
+    yield put({ type: 'FETCH_PREFERENCES' });
   } catch (err) {
     console.log('error in deletePreference', err);
   }
@@ -119,22 +120,22 @@ function* deletePreference(action) {
 
 function* editPreference(action) {
   try {
-    console.log('in editPreference with payload: ', action.payload);
+    // edit preference in db
     yield axios.put('/api/forms/edit_preference', action.payload);
-
-    yield put({type: 'FETCH_PREFERENCES'});
-  } catch(err) {
-    console.log('error in editPreference', err)
+    yield put({ type: 'FETCH_PREFERENCES' });
+  } catch (err) {
+    console.log('error in editPreference', err);
   }
 }
 
 function* fetchUsers() {
   try {
-    const response = yield axios.get('/api/forms/fetch_users')
-    console.log('in fetchUsers with response data: ', response.data)
-    yield put({ type: 'SET_USERS', payload: response.data })
+    // gets all users from db for admin panel
+    const response = yield axios.get('/api/forms/fetch_users');
+    // stores in reducer
+    yield put({ type: 'SET_USERS', payload: response.data });
   } catch (error) {
-    console.log('error in fetchUsers', error)
+    console.log('error in fetchUsers', error);
   }
 }
 
